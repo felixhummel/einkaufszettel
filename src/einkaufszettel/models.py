8ab00a695c0e4842cc02044ed6d9a0ea8cd3f2be
Felix Hummel <felix@felixhummel.de>
2025-08-04 21:40:43 +0200

    slugs are unique. kiss.

--- src/einkaufszettel/models.py
+++ src/einkaufszettel/models.py
@@ -3,31 +3,6 @@ from django.utils.text import slugify
 from .domain import DEFAULT_UNIT
 
 
-def generate_unique_slug(model_class, name, exclude_id=None, zettel=None):
-    """Generate a unique slug from name for the given model"""
-    base_slug = slugify(name)
-    if not base_slug:
-        base_slug = 'item'
-
-    slug = base_slug
-    counter = 1
-
-    while True:
-        queryset = model_class.objects.filter(slug=slug)
-
-        # For Item model, check uniqueness within the zettel
-        if zettel is not None:
-            queryset = queryset.filter(zettel=zettel)
-
-        # Exclude current instance when updating
-        if exclude_id:
-            queryset = queryset.exclude(id=exclude_id)
-
-        if not queryset.exists():
-            return slug
-
-        counter += 1
-        slug = f"{base_slug}-{counter}"
 
 
 class Zettel(models.Model):
@@ -38,10 +13,10 @@ class Zettel(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['slug']
 
     def save(self, *args, **kwargs):
-        self.slug = generate_unique_slug(Zettel, self.name, exclude_id=self.id)
+        self.slug = slugify(self.name, allow_unicode=True)
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -56,7 +31,7 @@ class Item(models.Model):
         on_delete=models.CASCADE
     )
     name = models.CharField(max_length=200)
-    slug = models.CharField(max_length=200, editable=False)
+    slug = models.SlugField(max_length=200, editable=False)
     qty = models.FloatField(default=1.0)
     unit = models.CharField(max_length=50, default=DEFAULT_UNIT)
     completed = models.BooleanField(default=False)
@@ -64,16 +39,16 @@ class Item(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['created_at']
+        ordering = ['slug']
         constraints = [
             models.UniqueConstraint(
                 fields=['zettel', 'slug'],
-                name='unique_item_slug_per_zettel'
+                name='unique_item_per_zettel'
             )
         ]
 
     def save(self, *args, **kwargs):
-        self.slug = generate_unique_slug(Item, self.name, exclude_id=self.id, zettel=self.zettel)
+        self.slug = slugify(self.name, allow_unicode=True)
         super().save(*args, **kwargs)
 
     def __str__(self):
